4a47a1be17278ebfe60a4d04c89b3c15538e7fdc
gedfalk <blite.co.stn@gmail.com>
2021-11-04 15:46:54 +0300

    ...

--- Algo/1_Stepik/2.2.6/step.py
+++ Algo/1_Stepik/2.2.6/step.py
@@ -12,3 +12,15 @@ fib(n)
 
 print('Вычисленные числа:', mas)
 print('Ваш ответ:', mas[n])
+
+
+
+def f(n):
+    mas = [0, 1]
+    res = []
+    for i in range(2, (n+1)*3):
+        mas.append(mas[i-1] + mas[i-2])
+        if i % 3 == 0:
+            res.append(mas[i])
+
+    print(*res, sep=',')
